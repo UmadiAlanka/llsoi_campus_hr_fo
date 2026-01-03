3b4fa8decf2b1b1bcb5ea42ca922dbdf15b0c19a
anduniInduja <anduniinduja@gmail.com>
2026-01-03 14:20:44 +0530

    Added Admin Add user page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import LoginForm from '@/app/login/LoginForm';
 import Dashboard from '@/app/admin-dashboard/Dashboard';
 import Attendance from '@/app/attendance/Attendance';
-import AdminManage from '@/app/admin-dashboard/admin-manage-users/adminManage';    
+import AdminManage from '@/app/admin-dashboard/admin-manage-users/adminManage';  
+import AdminAddUser from '@/app/admin-dashboard/admin-manage-users/adminAddUser';  
 
 export default function Home() {
   return (
    <div>
-     <AdminManage/>
+     <AdminAddUser/>
    </div>
   );
 }
