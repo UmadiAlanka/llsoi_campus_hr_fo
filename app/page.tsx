659491607f7e1eaab747be5e45b8cc1ab10f01c1
anduniInduja <anduniinduja@gmail.com>
2026-01-03 21:19:08 +0530

    Added Admin Attendance page UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Dashboard from '@/app/admin-dashboard/Dashboard';
 import Attendance from '@/app/attendance/Attendance';
 import AdminManage from '@/app/admin-dashboard/admin-manage-users/adminManage';  
 import AdminAddUser from '@/app/admin-dashboard/admin-manage-users/adminAddUser';
-import AdminEditUser from '@/app/admin-dashboard/admin-manage-users/adminEditUser';  
+import AdminAttendance from '@/app/admin-dashboard/admin-attendance/adminAttendance';  
 
 export default function Home() {
   return (
    <div>
-     <AdminEditUser/>
+     <AdminAttendance/>
    </div>
   );
 }
