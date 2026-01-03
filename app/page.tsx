fe539618f27953ab6ddedd65e9dbfc6728781457
anduniInduja <anduniinduja@gmail.com>
2026-01-03 15:07:29 +0530

    Added Admin Edit user page UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import LoginForm from '@/app/login/LoginForm';
 import Dashboard from '@/app/admin-dashboard/Dashboard';
 import Attendance from '@/app/attendance/Attendance';
 import AdminManage from '@/app/admin-dashboard/admin-manage-users/adminManage';  
-import AdminAddUser from '@/app/admin-dashboard/admin-manage-users/adminAddUser';  
+import AdminAddUser from '@/app/admin-dashboard/admin-manage-users/adminAddUser';
+import AdminEditUser from '@/app/admin-dashboard/admin-manage-users/adminEditUser';  
 
 export default function Home() {
   return (
    <div>
-     <AdminAddUser/>
+     <AdminEditUser/>
    </div>
   );
 }
